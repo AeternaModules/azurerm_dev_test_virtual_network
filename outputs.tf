d779a751b84621993dbf1d31ba0950878b6aa0b5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "dev_test_virtual_networks" {
-  description = "All dev_test_virtual_network resources"
-  value       = azurerm_dev_test_virtual_network.dev_test_virtual_networks
-}
 output "dev_test_virtual_networks_description" {
-  description = "List of description values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.description]
+  description = "Map of description values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.description }
 }
 output "dev_test_virtual_networks_lab_name" {
-  description = "List of lab_name values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.lab_name]
+  description = "Map of lab_name values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.lab_name }
 }
 output "dev_test_virtual_networks_name" {
-  description = "List of name values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.name]
+  description = "Map of name values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.name }
 }
 output "dev_test_virtual_networks_resource_group_name" {
-  description = "List of resource_group_name values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.resource_group_name]
+  description = "Map of resource_group_name values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.resource_group_name }
 }
 output "dev_test_virtual_networks_subnet" {
-  description = "List of subnet values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.subnet]
+  description = "Map of subnet values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.subnet }
 }
 output "dev_test_virtual_networks_tags" {
-  description = "List of tags values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.tags]
+  description = "Map of tags values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.tags }
 }
 output "dev_test_virtual_networks_unique_identifier" {
-  description = "List of unique_identifier values across all dev_test_virtual_networks"
-  value       = [for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : v.unique_identifier]
+  description = "Map of unique_identifier values across all dev_test_virtual_networks, keyed the same as var.dev_test_virtual_networks"
+  value       = { for k, v in azurerm_dev_test_virtual_network.dev_test_virtual_networks : k => v.unique_identifier }
 }
 
